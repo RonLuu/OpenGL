ddb7efd1b6a623838f384b405cae2811a8a4927f
Trieu Khai Luu <trieukhail@student.unimelb.edu.au>
2025-07-09 11:36:15 +1000

    Upload part 5

--- 5.Texture/Main.cpp
+++ 5.Texture/Main.cpp
@@ -1,69 +1,59 @@
-#include "Shader.h"
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <stb/stb_image.h>
 
+#include "Shader.h"
 #include <iostream>
-#include <math.h>
 
-// settings
 const unsigned int WIDTH = 800;
 const unsigned int HEIGHT = 600;
-
 void framebuffer_size_callback(GLFWwindow * window, int width, int height);
 void processInput(GLFWwindow * window);
+
 int main()
 {
-	// Create window
-#pragma region
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow * window = glfwCreateWindow(WIDTH, HEIGHT, "4.Shader", NULL, NULL);
+	GLFWwindow * window = glfwCreateWindow(WIDTH, HEIGHT, "5. Texture", NULL, NULL);
 	if (window == NULL)
 	{
-		std::cout << "Failed to create window\n";
+		std::cout << "Failed to create a window\n";
 		return -1;
 	}
+
 	glfwMakeContextCurrent(window);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-#pragma endregion
 
-	// Load Glad
-#pragma region
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	if (!gladLoadGLLoader(GLADloadproc(glfwGetProcAddress)))
 	{
-		std::cout << "Failed to initialize GLAD" << std::endl;
+		std::cout << "Failed to initialise GLAD\n";
 		return -1;
 	}
-#pragma endregion
 
-	Shader ourShader("x64/Debug/Shaders/shader.vert", "x64/Debug/Shaders/shader.frag");
+	Shader myShader("x64/Debug/Shaders/shader.vert", "x64/Debug/Shaders/shader.frag");
 
-	// Vertices
-#pragma region
 	float vertices[] = {
 		 0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,  // bottom right
 		-0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,  // bottom left
 		 0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f     // top 
 	};
-#pragma endregion
 
-	unsigned int VBO, VAO;
+	unsigned int VAO, VBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
 
 	glBindVertexArray(VAO);
-
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)(3*sizeof(float)));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
-	
+
 	while (!glfwWindowShouldClose(window))
 	{
 		processInput(window);
@@ -71,29 +61,30 @@ int main()
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		ourShader.use();
+		myShader.use();
 
 		glDrawArrays(GL_TRIANGLES, 0, 3);
-		
+
 		glfwSwapBuffers(window);
-        glfwPollEvents();
+		glfwPollEvents();
 	}
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
 
-    glfwTerminate();
-    return 0;
+	glfwTerminate();
+	return 0;
 }
 
-void processInput(GLFWwindow *window)
+void processInput(GLFWwindow * window)
 {
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
 }
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+
+void framebuffer_size_callback(GLFWwindow * window, int width, int height)
 {
-    // make sure the viewport matches the new window dimensions; note that width and 
-    // height will be significantly larger than specified on retina displays.
-    glViewport(0, 0, width, height);
+	// make sure the viewport matches the new window dimensions; note that width and 
+	// height will be significantly larger than specified on retina displays.
+	glViewport(0, 0, width, height);
 }
